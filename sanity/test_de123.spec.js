de99581f815917f3e4b109302b1fd25ac05e5cb3
Sharath LS <sharath.ls@feuji.com>
2026-06-05 18:36:25 +0530

    Update framework from UI

---
+++ sanity/test_de123.spec.js
@@ -0,0 +1,20 @@
+import testData from '../test-data.json';
+import { test, expect } from '@playwright/test';
+
+test('test_de123 @sanity', async ({ page }) => {
+  await page.goto(testData.url);
+  await page.waitForLoadState('domcontentloaded');
+
+  await expect(page.locator('[data-test="username"]')).toBeEnabled();
+  await page.locator('[data-test="username"]').click();
+  await expect(page.locator('[data-test="username"]')).toBeEditable();
+  await page.locator('[data-test="username"]').fill(testData.username);
+
+  await expect(page.locator('[data-test="password"]')).toBeEnabled();
+  await page.locator('[data-test="password"]').click();
+  await expect(page.locator('[data-test="password"]')).toBeEditable();
+  await page.locator('[data-test="password"]').fill(testData.password);
+
+  await expect(page.locator('[data-test="login-button"]')).toBeEnabled();
+  await page.locator('[data-test="login-button"]').click();
+});
