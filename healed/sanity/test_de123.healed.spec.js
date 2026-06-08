a658ad5e1462060e94dac41d0998385203f015d7
Sharath LS <sharath.ls@feuji.com>
2026-06-08 12:56:35 +0530

    Update framework from UI

--- healed/sanity/test_de123.healed.spec.js
+++ healed/sanity/test_de123.healed.spec.js
@@ -6,23 +6,16 @@ test('test_de123 @sanity', async ({ page }) => {
   await page.goto(testData.url);
   
   await page.waitForLoadState('domcontentloaded');
+  await page.waitForLoadState('domcontentloaded');
+  await heal(page, 'username field', 'click', null, () => page.locator('[data-test="username"]'));
+  await heal(page, 'username field', 'click', null, () => page.locator('[data-test="username"]'));
+  await heal(page, 'username field', 'click', null, () => page.locator('[data-test="username"]'));
+  await heal(page, 'username field', 'fill', testData.username, () => page.locator('[data-test="username"]'));
 
-  await heal(page, 'username field', 'click', null,
-    () => page.locator('[data-test="username"]'));
-  await heal(page, 'username field', 'click', null,
-    () => page.locator('[data-test="username"]'));
-  await heal(page, 'username field', 'fill', testData.username,
-    () => page.locator('[data-test="username"]'));
+  await heal(page, 'password field', 'click', null, () => page.locator('[data-test="password"]'));
+  await heal(page, 'password field', 'fill', testData.password, () => page.locator('[data-test="password"]'));
 
-  await heal(page, 'password field', 'click', null,
-    () => page.locator('[data-test="password"]'));
-  await heal(page, 'password field', 'fill', testData.password,
-    () => page.locator('[data-test="password"]'));
-
-  await heal(page, 'login button', 'click', null,
-    () => page.locator('[data-test="login-button"]'));
-  await heal(page, 'login button', 'click', null,
-    () => page.locator('[data-test="login-button"]'));
-  await heal(page, 'login button', 'click', null,
-    () => page.locator('[data-test="login-button"]'));
+  await heal(page, 'login button', 'click', null, () => page.locator('[data-test="login-button"]'));
+  await heal(page, 'login button', 'click', null, () => page.locator('[data-test="login-button"]'));
+  await heal(page, 'login button', 'click', null, () => page.locator('[data-test="login-button"]'));
 });
